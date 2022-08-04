ec56960d9e21e44def6b74218000f119a12d064f
hamidadem <abduhaman46@gmail.com>
2022-08-04 05:51:22 -0700

    YAYA

---
+++ 0x10-variadic_functions/3-main.c
@@ -0,0 +1,12 @@
+nclude "variadic_functions.h"
+
+/**
+ *  * main - check the code for ALX School students.
+ *   *
+ *    * Return: Always 0.
+ *     */
+int main(void)
+{
+	    print_all("ceis", 'H', 0, "lberton");
+	        return (0);
+}
